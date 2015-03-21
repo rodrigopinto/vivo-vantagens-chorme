c399f15e2e27a45af941ea4064d87ceb83faeb11
Rodrigo Pinto <rodrigopqn@gmail.com>
2015-03-21 20:31:30 -0300

    Update to avoid status different of available.

--- vivo-vantagens.js
+++ vivo-vantagens.js
@@ -1,7 +1,11 @@
 var tds = document.querySelectorAll("tr > td.disponibilidade");
 
+var availability = "";
+
 for (i = 0; i < tds.length; i++) {
-  if (tds[i].innerHTML.trim() == "Esgotado"){
+  availability = tds[i].innerHTML.trim();
+
+  if (availability == "Esgotado" || availability == "reservas encerradas") {
     var tr = tds[i].parentElement;
     tr.parentElement.removeChild(tr);
   }
